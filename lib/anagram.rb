8cf8be2e9aad5ea4441d315e685a261bd9c22ad2
Judy McGoldrick <jjeanius@verizon.net>
2018-01-12 01:59:40 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+require'pry'
+
+class Anagram
+  attr_accessor :words
+
+  def initialize(words)
+    @words = words
+  end
+
+  def match(words)
+    words.detect do |word|
+      if word != words
+        words =[]
+        else
+          words.split()
+
+         end
+         end
+        end
+    end
