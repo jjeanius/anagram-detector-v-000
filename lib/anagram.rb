bcf2baa908c8ca208f7aeddbd269ec5defd60485
Judy McGoldrick <jjeanius@verizon.net>
2018-01-12 02:20:03 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,18 +1,18 @@
 require'pry'
 
 class Anagram
-  attr_accessor :words
+  attr_accessor :anagrams, :words
 
   def initialize(words)
     @words = words
 
   end
 
-  def match(words)
-    words.new
-    words.detect do |word|
-      if word == words
-      words.split
+  def match(anagrams)
+    anagrams.new
+    anagrams.detect do |word|
+      if word == anagrams
+      anagrams.split
         end
     end
 end
